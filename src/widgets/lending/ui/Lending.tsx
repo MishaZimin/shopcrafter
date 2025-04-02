ae9b406bab620b4fcb606911471f8c5906e324bf
ZiminMisha <mikhail.zimin.2004@bk.ru>
2025-04-02 13:43:47 +0500

    .

--- src/widgets/lending/ui/Lending.tsx
+++ src/widgets/lending/ui/Lending.tsx
@@ -29,9 +29,9 @@ export default function Lending() {
       <section id="price" className="pb-16">
         <h1 className="pb-2">Цены</h1>
         <div className="w-full flex flex-row gap-4  rounded-xl">
-          <div className=" bg-gray-100 rounded-xl flex-1 h-[300px]" />
-          <div className=" bg-gray-100 rounded-xl h-[300px] flex-1" />
-          <div className=" bg-gray-100 rounded-xl h-[300px] flex-1" />
+          <div className=" bg-gray-100 rounded-xl flex-1 h-[600px]" />
+          <div className=" bg-gray-100 rounded-xl h-[600px] flex-1" />
+          <div className=" bg-gray-100 rounded-xl h-[600px] flex-1" />
         </div>
       </section>
     </div>
